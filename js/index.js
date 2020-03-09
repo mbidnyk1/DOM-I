fd83503cd04811aae4ac2b5cf3cb708959b8b77e
Michael Bidnyk <mbidnyk1@gmail.com>
2020-03-09 18:08:43 -0500

    added src images

--- js/index.js
+++ js/index.js
@@ -40,3 +40,9 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.setAttribute('src',siteContent['cta']['img-src'])
+
+const middleImg = document.querySelector('.middle-img');
+middleImg.setAttribute('src',siteContent['main-content']['middle-img-src'])
